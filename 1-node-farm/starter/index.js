f92d84ed2e57e0521f76a54ffe87dff399b01c22
Vladislav Shevtsov <vladshevtsov56@gmail.com>
2024-12-19 18:00:28 +0100

    feat(added): async read and write txt file

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,32 @@
 const fs = require("fs");
 
-const textInput = fs.readFileSync("./txt/input.txt", {
-  encoding: "utf-8",
-});
-console.log(textInput);
-const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+// Blocking execution
+// const textInput = fs.readFileSync("./txt/input.txt", {
+//   encoding: "utf-8",
+// });
+// console.log(textInput);
 
-fs.writeFileSync("./txt/textOutput", textOutput);
-console.log("File written!");
+// const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/textOutput", textOutput);
+// console.log("File written!");
+
+// Unblocking execution
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) {
+    // change path to txt/start
+    console.log("Error!");
+    return;
+  }
+
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(`From data1: ${data1} -> ${data2}`);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("File has been written");
+      });
+    });
+  });
+});
+console.log("first console");
