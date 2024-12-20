82de2b6536e2975121447c8d4cc85fddfcf254de
Vladislav Shevtsov <vladshevtsov56@gmail.com>
2024-12-20 15:53:49 +0100

    feat(added): http module intro

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,7 @@
-const fs = require("fs");
+// const fs = require("fs");
+const http = require("http");
+
+// FILES ////////////////////////////////////////////////////////////////
 
 // Blocking execution
 // const textInput = fs.readFileSync("./txt/input.txt", {
@@ -11,22 +14,33 @@ const fs = require("fs");
 // console.log("File written!");
 
 // Unblocking execution
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) {
-    // change path to txt/start
-    console.log("Error!");
-    return;
-  }
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) {
+//     // change path to txt/start
+//     console.log("Error!");
+//     return;
+//   }
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(`From data1: ${data1} -> ${data2}`);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(`From data1: ${data1} -> ${data2}`);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("File has been written");
-      });
-    });
-  });
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("File has been written");
+//       });
+//     });
+//   });
+// });
+// console.log("first console");
+
+// SERVER ////////////////////////////////////////////////////////////////
+
+const server = http.createServer((req, res) => {
+  console.log("req", req.headers);
+  res.end("Hello from the server");
 });
-console.log("first console");
+
+server.listen(8000, () => {
+  console.log("Start listening on port 8000");
+});
