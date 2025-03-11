ef69307cf95f2bace57ef9e750e32ef111b4d2a5
Vladislav Shevtsov <vladshevtsov56@gmail.com>
2025-03-11 17:49:28 +0100

    feat(added): [4] handle POST request

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json()); // Middleware
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -15,6 +17,33 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log('=========================================');
+  console.log('req.body', req.body);
+  console.log('=========================================');
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+      }
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const PORT = 3000;
