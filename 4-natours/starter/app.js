c1e996f048f24d28020d750eb0f9982c7dadcd2f
Vladislav Shevtsov <vladshevtsov56@gmail.com>
2025-03-11 18:00:49 +0100

    feat(added): [4] handle GET request with ID

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -19,6 +19,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id/:optionalId?', (req, res) => {
+  const { id } = req.params;
+  const tour = tours.find((tour) => tour.id === Number(id));
+
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+    return;
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   console.log('=========================================');
   console.log('req.body', req.body);
