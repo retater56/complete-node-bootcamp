8dd4b58a8a88eb02b1bf11bcd2f1e07d297b49f1
Vladislav Shevtsov <vladshevtsov56@gmail.com>
2025-03-21 16:54:38 +0100

    feat(added): [4] eslint plugins

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -7,7 +7,7 @@ console.log('=========================================');
 console.log(process.env);
 console.log('=========================================');
 
-const port = process.env.PORT ?? 1234;
+const port = process.env.PORT || 1234;
 app.listen(port, () => {
   console.log('=========================================');
   console.log('Listen port: ', port);
